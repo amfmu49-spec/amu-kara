42abc460330f51594b25e485ff29cb29db48f38e
amfmu49-spec <amfmu49-spec@users.noreply.github.com>
2026-08-01 00:45:53 +0900

    Updates

--- _next/static/lVs-S72pAq5woYEjGaZeM/_buildManifest.js
+++ _next/static/lVs-S72pAq5woYEjGaZeM/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/amu-kara//_next/:path+",
-        "destination": "/amu-kara/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
